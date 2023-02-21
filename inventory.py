1f2c5e41b832a5bfa72bb05778405442ff5450de
TomaszKamola <tomasz94kamola@gmail.com>
2023-02-21 19:17:49 +0100

    DB models added

--- inventory.py
+++ inventory.py
@@ -1,33 +1,34 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
-table_data = {
-    'name': [
-        'Random1',
-        'Random2',
-        'Random3'
-    ],
-    'value': [
-        12.34,
-        34.56,
-        56.78
-    ],
-    'description': [
-        'Some text for random1',
-        'Some text for random2',
-        'Some text for random3'
-    ]
-}
+# table_data = {
+#     'name': [
+#         'Random1',
+#         'Random2',
+#         'Random3'
+#     ],
+#     'value': [
+#         12.34,
+#         34.56,
+#         56.78
+#     ],
+#     'description': [
+#         'Some text for random1',
+#         'Some text for random2',
+#         'Some text for random3'
+#     ]
+# }
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
 @app.route("/")
 @app.route("/home")
 def hello():
     return render_template(
-        'home.html', 
-        table_data=table_data, 
-        table_len=len(table_data)
+        'home.html'
     )
 
 @app.route("/about")
